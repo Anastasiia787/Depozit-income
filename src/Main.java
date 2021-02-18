203d481d4f5f733d48a4d28b55f6da5236a720e2
Anastasia <nastenaprokopeva78@gmail.com>
2021-02-18 23:06:45 +0300

    sum update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int sum = 1000;
+        int sum = 19999999;
         int persent = 3;
         System.out.println(sum * persent /100);
     }
