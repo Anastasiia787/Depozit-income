6052fe759175861def141ea481a781c90982a701
Anastasia <nastenaprokopeva78@gmail.com>
2021-02-19 20:46:03 +0300

    result

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int sum = 19999999;
         int persent = 3;
-        System.out.println(sum * persent /100);
+        int income = sum * persent / 100;
+        System.out.println(income);
     }
 }
